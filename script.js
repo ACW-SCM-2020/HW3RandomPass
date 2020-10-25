0de93ce8a486ee7b4b2d8dcaa30e885638e4d42e
Andrew Wilkinson <andrewscm2020@gmail.com>
2020-10-24 23:04:49 -0700

    At this stage, I have written out all of what I believe would be the required prompts and confirm statements to allow for basic dialouge between the user and the selections they could make, while still allowing space incase I need to go back and add additional portions. I know a switch statement is going to be required at some level, as we now have an output that will require choosing between one of the four character types depending on whether or not the user wants that in their password. I also tried to research how to make a disabled status to a javascript function to see if I could simply always output all 4, and if disabled, loop over the ones that are still enabled until the chosen character quantity would be satisfied (within a for loop). Determining now what is my next course of action.

--- script.js
+++ script.js
@@ -6,6 +6,22 @@ var lowercaseArray = ["abcdefghijklmnopqrstuvwxyz"];
 var specialArray = ["!@^$%#("];
 var numericalArray = ["0123456789"];
 
+var question1 = false;
+var q1prompt = false;
+
+
+var question2 = false;
+var q2confirm = false;
+
+var question3 = false;
+var q3confirm = false;
+
+var question4 = false;
+var q4confirm = false;
+
+var question5 = false;
+var q5confirm = false;
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
@@ -18,11 +34,11 @@ function writePassword() {
 }
 
 function uppercase() {
-    return uppercaseArray[(Math.floor(Math.random() * Math.floor(26)))]
+        return uppercaseArray[(Math.floor(Math.random() * Math.floor(26)))]
 }
 
 function lowercase() {
-    return lowercaseArray[(Math.floor(Math.random() * Math.floor(26)))]
+        return lowercaseArray[(Math.floor(Math.random() * Math.floor(26)))]
 }
 
 function special() {
@@ -34,12 +50,49 @@ function numerical() {
 }
 
 function generatePassword() {
-  var question1 = prompt("How many characters would you like in your password between 8 and 128?");
-  var question2 = confirm("Would you like Uppercase letters in your password?");
-  var question3 = confirm("Would you like Lowercase letters in your password?");
-  var question4 = confirm("Would you like Special characters in your password?");
-  var question5 = confirm("Would you like Numerical characters in your password?")    
+    question1 = prompt("How many characters would you like in your password between 8 and 128?");
+    if (question1 < 8 || question1 > 128) {
+        alert("Please input a valid character quantity.");}
+    else {
+        q1prompt = true;
+    } 
+    question2 = confirm("Would you like Uppercase letters in your password?");
+    if (question2 = true) {
+        q2confirm = true;
+    }
+    question3 = confirm("Would you like Lowercase letters in your password?");
+    if (question3 = true) {
+        q3confirm = true;
+    }
+    question4 = confirm("Would you like Special characters in your password?");
+    if (question4 = true) {
+        q4confirm = true;
+    }
+    question5 = confirm("Would you like Numerical characters in your password?");
+    if (question5 = true) {
+        q5confirm = true;
+    }
+    if (q1prompt = true && q2confirm === true && q3confirm === true && q4confirm === true && q5confirm === true){
+        return ()
+    }
+
+function singlechar(){
+    thisCharacter = charchoice();
+    switch(thisCharacter){
+    case 1:
+        return uppercase();
+    case 2:
+        return lowercase();
+    case 3:
+        return special();
+    case 4:
+        return numerical();
+        }
 }
+
+}
+
+
 
 
 
