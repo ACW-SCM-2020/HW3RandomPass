71fe5febc785fc7716fd5823f263ff9481e1916c
Andrew Wilkinson <andrewscm2020@gmail.com>
2020-10-24 18:02:27 -0700

    At this stage, I knew that I had to make some sort of function for each of the possible constraints that we will need to apply in different switch-cases. Basically, I just wanted the mechanism of having being able to randomly select either a lowercase letter, uppercase letter, special character, or numerical character with a math.random function. I still don't exactly know how I will implement this moving forward, but my guess is that I will have to make a switch-case that will determine how many times I randomly select characters between which of the 4 character types can be applied. Now, I created the prompts and confirms which I believe I can then start creating a results function with depending on the user's inputs.

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-var uppercase = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
-var lowercase = ["abcdefghijklmnopqrstuvwxyz"];
-var special = ["!@^$%#("];
-var numerical = ["0123456789"];
+var uppercaseArray = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+var lowercaseArray = ["abcdefghijklmnopqrstuvwxyz"];
+var specialArray = ["!@^$%#("];
+var numericalArray = ["0123456789"];
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
@@ -17,15 +17,30 @@ function writePassword() {
 
 }
 
+function uppercase() {
+    return uppercaseArray[(Math.floor(Math.random() * Math.floor(26)))]
+}
+
+function lowercase() {
+    return lowercaseArray[(Math.floor(Math.random() * Math.floor(26)))]
+}
+
+function special() {
+    return specialArray=[(Math.floor(Math.random() * Math.floor(7)))]
+}
+
+function numerical() {
+    return numericalArray=[(Math.floor(Math.random() * Math.floor(10)))]
+}
 
 function generatePassword() {
   var question1 = prompt("How many characters would you like in your password between 8 and 128?");
-  if (question1 > 8 && question1 < 128) {
-    return case_2;
-  elseif (question1 < 8 || question1 > 128) 
-    
-  }
+  var question2 = confirm("Would you like Uppercase letters in your password?");
+  var question3 = confirm("Would you like Lowercase letters in your password?");
+  var question4 = confirm("Would you like Special characters in your password?");
+  var question5 = confirm("Would you like Numerical characters in your password?")    
 }
+
 
 
 
